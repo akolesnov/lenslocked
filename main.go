187f53153df292bbf91ebfc4e3ed0c52dcf1280c
akolesnov <a.kolesnov@yandex.ru>
2025-07-24 15:16:51 +0300

    Added embedding views

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/akolesnov/lenslocked/controllers"
+	"github.com/akolesnov/lenslocked/templates"
 	"github.com/akolesnov/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
-	// Or inline everything and skip the `tpl` variable.
+
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
